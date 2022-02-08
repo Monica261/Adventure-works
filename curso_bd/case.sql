1324dbd38d4f3bf870adbe18bb0639bba478563a
mnstorres <mnstorres@santacasasaudesjc.com.br>
2022-02-08 11:13:43 -0300

    utilização do case com expressão lógica

--- curso_bd/case.sql
+++ curso_bd/case.sql
@@ -31,4 +31,48 @@ delimiter ;
 
 drop procedure prc_case_exemplo;
 
-call prc_case_exemplo(2);
+call prc_case_exemplo(3);
+
+/*VERIFICAR IDADE COM CASE*/
+delimiter $$
+create procedure prc_case_idade(in idade float)
+comment 'verificar idade do usuario a partir de case'
+begin
+	case /*quando for para verificar condicional(expressão logica), não precisa passar a var no case*/
+		when idade < 18 then select 'criança';
+		when idade >= 18 and idade < 23 then select 'jovem';
+        when idade > 23 and idade < 50 then select 'adulto';
+        when idade > 50 then select 'idoso';
+	else
+		select 'insira uma idade';
+	end case;
+end;
+$$
+delimiter ;
+
+drop procedure prc_case_idade;
+
+call prc_case_idade(15)
+
+/*VERIFICAR DATA ATUAL NO BANCO COM CASE*/
+delimiter $$
+create procedure prc_data_atual()
+comment 'verificando data atual no banco'
+begin
+	declare data_teste date;
+	set data_teste = curdate() + 1; /*+1 - futuro	-1 - passado*/
+    
+    case
+		when data_teste = curdate() then select 'presente' as data_teste;
+        when data_teste < curdate() then select 'passado' as data_teste;
+        when data_teste > curdate() then select 'futuro' as data_teste;
+	else
+		select 'nenhuma das anterioes';
+	end case;
+end
+$$
+delimiter ;
+
+drop procedure prc_data_atual;
+
+call prc_data_atual();
