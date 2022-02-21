ee1362018a739259128206d448ac4b7ea9e0f928
Monica261 <desenvolvimento.midiasim@gmail.com>
2022-02-20 21:25:32 -0300

    add loop

--- curso_bd/cursores.sql
+++ curso_bd/cursores.sql
@@ -54,30 +54,62 @@ delimiter $$
 create procedure proc_boleto()
 comment 'procedure para processamento de contrato e boleto'
 begin
-	declare v_fk_idcontrato, v_fk_idaluno, v_fk_idcurso int;
+	declare v_fim_loop int default 0;
+    declare v_log text;
+	declare v_idcontrato, v_fk_idaluno, v_fk_idcurso int;
     declare v_fk_data_inscricao_curso datetime;
     declare v_valor_total_curso float(8,2);
-    declare v_desconto float(4,1);
+	declare v_desconto float(4,1);
     declare v_numero_parcelas int;
     declare v_processado char(1);
+	
+    declare v_boleto_loop_parcelas int;
+    declare v_boleto_valor_parcela float(8,2);
+	declare v_boleto_data_vencimento datetime;
+    
     
     declare c_contratos cursor for(-- var do tipo cursor
 		select idcontrato, fk_idaluno, fk_idcurso, fk_data_inscricao_curso, valor_total_curso, desconto, numero_parcelas, processado
         from contrato
     );
     
+    /*manipulando handler de continuidade do laço */
+    declare continue handler for not found set v_fim_loop = 1; -- quando não houver mais elementos, essa instrução será executada
+    
     -- abrir o cursor
     open c_contratos;
      
-     fetch c_contratos into v_fk_idcontrato, v_fk_idaluno, v_fk_idcurso, v_fk_data_inscricao_curso, v_valor_total_curso, v_desconto, v_numero_parcelas,
-     v_processado;
-     
-     select v_fk_idcontrato, v_fk_idaluno, v_fk_idcurso, v_fk_data_inscricao_curso,
-     concat('$', format(v_valor_total_curso, 2)) as "v_valor_total_curso",
-     v_desconto, v_numero_parcelas,
-     case 
-		when v_processado = 'n' then 'NAO'
-     end as "v_processado";
+     repeat
+		 -- utilizar uma estrutura de repetição para evitar a repetição do fetch
+		 fetch c_contratos into v_idcontrato, v_fk_idaluno, v_fk_idcurso, v_fk_data_inscricao_curso, v_valor_total_curso, v_desconto, v_numero_parcelas,
+		 v_processado;
+		 
+         if v_fim_loop = 0 then -- evita que o ultimo elemento do cursor se repita
+			 set v_log = concat(
+				v_fim_loop, ' *** ', v_idcontrato, ' - ', v_fk_idaluno, ' - ', v_fk_idcurso, ' - ', v_fk_data_inscricao_curso, ' - ',
+				v_valor_total_curso, ' - ', v_desconto, ' - ', v_numero_parcelas
+			 );
+			 
+			insert into log(log) value(v_log);
+            
+            /*estrutura de repetição para inserir as parcelas de acordo com o valor da mensalidade*/
+            while v_boleto_loop_parcelas <= v_numero_parcelas do
+				insert into boleto(fk_idcontrato, data_vencimento, valor, numero_parcela)
+				values(v_idcontrato, v_boleto_data_vencimento, v_boleto_valor_parcela, v_boleto_loop_parcelas);
+            
+				set v_boleto_loop_parcelas = v_boleto_loop_parcelas + 1;
+            end while;
+            
+         end if;
+         
+		 select v_idcontrato, v_fk_idaluno, v_fk_idcurso, v_fk_data_inscricao_curso,
+		 concat('$', format(v_valor_total_curso, 2)) as "v_valor_total_curso",
+		 v_desconto, v_numero_parcelas,
+		 case 
+			when v_processado = 'n' then 'NAO'
+		 end as "v_processado";
+         
+     until v_fim_loop > 0 end repeat; /*quando a condição for verdadeira, o laço será interrompido*/
      
     -- fechar o cursor
     close c_contratos;
@@ -90,3 +122,8 @@ call proc_boleto();
 drop procedure proc_boleto;
 
 select * from contrato; 
+
+select * from log;
+select * from boleto;
+
+truncate table log;
