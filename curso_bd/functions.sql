0e3f863b4969e4db8cb96e824734f9dce4e5e2c9
Monica261 <desenvolvimento.midiasim@gmail.com>
2022-02-20 21:33:21 -0300

    funções em sql

---
+++ curso_bd/functions.sql
@@ -0,0 +1,10 @@
+-- Funções são rotinas que retornam valores ou tabelas
+
+-- modelo
+create function func_modelo()
+returns <tipo_de_dados_que_sera_retornado>
+begin
+	
+    /*bloco de codigo da funcao*/
+    return <retorno>
+end;
