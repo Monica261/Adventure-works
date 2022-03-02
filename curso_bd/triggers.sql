8f285452ecc832a403dea5582efe4c70601cf4e3
Monica261 <desenvolvimento.midiasim@gmail.com>
2022-03-01 22:39:02 -0300

    trigger before insert e after insert

--- curso_bd/triggers.sql
+++ curso_bd/triggers.sql
@@ -1,12 +1,16 @@
 /*Trigger - Before Insert*/
+use universidade_u;
 
 -- modelo de sintaxe
+delimiter $$
 create trigger trg_nome
 before insert on nome_da_tabela
 for each row -- para cada linha
 begin
 	
 end;
+$$
+delimiter ;
 
 /*preparo do ambiente*/
 use universidade_u;
@@ -21,3 +25,50 @@ create table funcionario_log (
     telefone_antigo varchar(20),
     telefone_novo varchar(20)
 );
+
+-- criando trigger before insert
+delimiter $$
+create trigger trg_funcionario_before_insert
+before insert on funcionario
+for each row
+begin
+	insert into funcionario_log(fk_idmatricula, tipo_log, funcao_antiga, funcao_nova, telefone_antigo, telefone_novo)
+    values(null, 'BI', null, NEW.funcao, null, NEW.telefone);
+end
+$$
+delimiter ;
+
+drop trigger trg_funcionario_before_insert;
+
+INSERT INTO universidade_u.funcionario(telefone, funcao, nome) VALUES ('98524-8605', 'Dev Junior', 'Pedro');
+
+select * from funcionario;
+select * from funcionario_log;
+
+-- mostrar triggers existentes no banco
+show triggers;
+
+-- verificar a instrução da trigger
+show create trigger universidade_u.trg_funcionario_before_insert;
+
+-- remover trigger
+drop trigger universidade_u.trg_funcionario_before_insert;
+
+-- listar trigger de uma determinada tabela
+show triggers where `table` = 'funcionario';
+
+/*Trigger - after insert*/
+delimiter $$
+create trigger trg_funcionario_after_insert
+after insert on funcionario
+for each row
+begin
+	select 'DADOS INSERIDOS';
+end
+$$
+delimiter ;
+
+INSERT INTO universidade_u.funcionario(telefone, funcao, nome) VALUES ('98256-7432', 'Dev Sênior', 'Maria');
+
+select * from funcionario;
+select * from funcionario_log;
