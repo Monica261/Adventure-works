babe7f4d35d9f85defac11f4ba58f0d145b0d873
mnstorres <mnstorres@santacasasaudesjc.com.br>
2022-04-04 16:37:53 -0300

    bd padrão - performance schema

--- curso_bd/bd_mysql.sql
+++ curso_bd/bd_mysql.sql
@@ -4,15 +4,34 @@ use mysql;
 
 show tables;
 
-/*permissões de usuarios*/
+-- permissões de usuarios
 select * from user;
 
-/*status das tabelas do bd*/
+-- status das tabelas do bd
 select * from innodb_table_stats;
 
-/*logs do banco*/
+-- logs do banco
 select * from slow_log;
 select * from general_log;
 
 -- topicos para ajudar usuario
-select * from help_topic where name like 'JOIN%'
+select * from help_topic where name like 'JOIN%';
+
+/*BD PADRÃO - PERFORMANCE SCHEMA
+Tabelas que armazenam infos de execução do servidor mysql em baixo nivel:
+-logs de eventos do servidor em tempo de execução
+-desempenho - performance
+-conexões ativas de hosts e usuarios
+*/
+show databases;
+
+use performance_schema;
+
+-- inspecionar as variaveis de sistema e de status
+select * from global_status;
+select * from global_variables;
+
+-- analisar os clientes conectados ao servidor
+select * from hosts;
+
+show tables;
