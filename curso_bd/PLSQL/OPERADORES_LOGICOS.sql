31b397a37160283ed74d1bab6929e7964e529e48
mnstorres <mnstorres@santacasasaudesjc.com.br>
2022-06-23 10:34:55 -0300

    operadores logicos or, where, like, in, not in e etc

--- curso_bd/PLSQL/OPERADORES_LOGICOS.sql
+++ curso_bd/PLSQL/OPERADORES_LOGICOS.sql
@@ -23,3 +23,44 @@ pib_per_cap decimal(12,3) not null
 select user from dual;
 
 select * from senso;
+
+select * from HR.countries;
+
+--OPERADORES LOGICOS
+ 
+ --where
+ select * from HR.countries co
+ where co.country_name = 'Brazil'
+ 
+ --and
+ select * from HR.employees emp
+ where emp.salary >= 17000
+ and emp.manager_id = 100
+ 
+ --in
+ select count(*), emp.first_name 
+ from hr.employees emp
+ where emp.job_id in ('IT_PROG', 'AD_VP')
+ group by emp.first_name 
+ 
+ --not in
+ select * from HR.employees emp
+ where emp.manager_id not in (100)
+ 
+ --between
+ select count(emp.DEPARTMENT_ID) || ' - ' || emp.first_name quant_for_depatment from HR.employees emp
+ where emp.hire_date between to_date('25-JUN-05', 'DD/MM/YY') and to_date('30-NOV-08', 'DD/MM/YY')
+ group by emp.first_name --quant de nomes por departamento                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+ 
+ --not like
+  select * from HR.employees emp
+  where emp.first_name not like '%a%'
+  and emp.job_id = 'IT_PROG'
+  
+  --or
+  select * from HR.employees emp
+  where emp.first_name not like '%a%'
+  and (emp.job_id = 'IT_PROG' or emp.job_id = 'FI_ACCOUNT')
+  
+  
+ 
